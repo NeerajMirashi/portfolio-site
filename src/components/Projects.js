d20b2019773fecb90ec51ba22d9034650129bdcb
Neeraj Mirashi <96018344+NeerajMirashi@users.noreply.github.com>
2023-09-04 10:59:15 +0530

    Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ import nexus from "../assets/img/nexus.jpg";
 import encipher from "../assets/img/encipher.png";
 import stem from "../assets/img/stem.jpg";
 import aws from "../assets/img/aws.jpg";
-import web3.0 from "../assets/img/web3.0.jpg";
+import web3 from "../assets/img/web3.jpeg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -35,7 +35,7 @@ export const Projects = () => {
       tech: "ReactJS | Tailwind-CSS | Metamask | Solidity",
       link: <a href="https://youtu.be/_i2uJO1Jv-U" target="_blank">link</a>,
       description: "Deployed a modern Web 3.0 Blockchain App that performs real life transactions",
-      imgUrl: web3.0,
+      imgUrl: web3,
     },  
   ];
 
